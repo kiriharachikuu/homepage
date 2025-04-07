fd38a7b6e05b2d6d63669a7c534dd2b775251fd8
桐原知空 <42866646+kiriharachikuu@users.noreply.github.com>
2025-04-07 23:31:09 +0800

    Update update.ts

--- src/data/update.ts
+++ src/data/update.ts
@@ -1,18 +1,18 @@
 export default [
   {
     className: "zhihu",
-    updateUrl: "https://api.swo.moe/stats/zhihu/cuicuijiang",
+    updateUrl: "https://api.swo.moe/stats/weibo/5574382615",
   },
   {
     className: "bili",
-    updateUrl: "https://api.swo.moe/stats/bilibili/5044093",
+    updateUrl: "https://bili-count-api.chikuu.top/api/count?vmid=28826850",
   },
   {
     className: "github",
-    updateUrl: "https://api.swo.moe/stats/github/idealclover",
+    updateUrl: "https://api.swo.moe/stats/github/kiriharachikuu",
   },
   {
     className: "netease",
-    updateUrl: "https://api.swo.moe/stats/neteasemusic/78983390",
+    updateUrl: "https://api.swo.moe/stats/neteasemusic/379188047",
   },
 ];
